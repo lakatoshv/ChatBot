cc8d873d45543abfe50b87cf7bb01aa224321110
Віталій Лакатош <some.pochta1@gmail.com>
2021-06-28 00:08:37 +0300

    Add default implementations for message data

---
+++ ChatBot/Tests/ChatBot.UnitTests/AutomatedMessagingTests/DataForTests/AlwaysReadyMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChatBot.Core.Interfaces;
+
+namespace ChatBot.UnitTests.AutomatedMessagingTests.DataForTests
+{
+    /// <summary>
+    /// Always ready message.
+    /// </summary>
+    /// <seealso cref="IAutomatedMessage" />
+    public class AlwaysReadyMessage : IAutomatedMessage
+    {
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public void Initialize(DateTime currentTime)
+        {
+        }
+
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public bool IsItTimeToDisplay(DateTime currentTime)
+        {
+            return true;
+        }
+
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public string GetMessage(DateTime currentTime)
+        {
+            return "Always ready";
+        }
+    }
+}
---
+++ ChatBot/Tests/ChatBot.UnitTests/AutomatedMessagingTests/DataForTests/NeverReadyMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChatBot.Core.Interfaces;
+
+namespace ChatBot.UnitTests.AutomatedMessagingTests.DataForTests
+{
+    /// <summary>
+    /// Never ready message.
+    /// </summary>
+    /// <seealso cref="IAutomatedMessage" />
+    public class NeverReadyMessage : IAutomatedMessage
+    {
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public void Initialize(DateTime currentTime)
+        {
+        }
+
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public bool IsItTimeToDisplay(DateTime currentTime)
+        {
+            return false;
+        }
+
+        /// <inheritdoc cref="IAutomatedMessage"/>
+        public string GetMessage(DateTime currentTime)
+        {
+            throw new NotImplementedException("Something went wrong!");
+        }
+    }
+}
