beb2204ba7771840b7b0073a9d08e32f78a1da0f
Віталій Лакатош <some.pochta1@gmail.com>
2021-06-27 23:41:22 +0300

    Add publish should test with automated the message to managed message

---
+++ ChatBot/Tests/ChatBot.UnitTests/AutomatedMessagingTests/AutomatedMessagingSystemTests/PublishShouldTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ChatBot.Core.AutomatedMessaging;
+using Xunit;
+
+namespace ChatBot.UnitTests.AutomatedMessagingTests.AutomatedMessagingSystemTests
+{
+    public class PublishShouldTest
+    {
+        [Fact]
+        public void AutomatedMessageToManagedMessage()
+        {
+            var messagingSystem = new AutomatedMessagingSystem();
+            var intervalTriggeredMessage = new IntervalTriggeredMessage
+            {
+                DelayInMinutes = 1,
+                Message = "Welcome! If you are enjoying the content, please follow this channel!",
+            };
+            intervalTriggeredMessage.Initialize(DateTime.Now);
+            messagingSystem.Publish(intervalTriggeredMessage);
+
+            Assert.Contains(intervalTriggeredMessage, messagingSystem.ManagedAutomatedMessages);
+        }
+    }
+}
